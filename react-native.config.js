2e2cb9b2a48d365ef7d1493e56d43ba13f89942a
Daniel Kamar <kamarster@gmail.com>
2020-02-28 17:59:09 +0300

    Deployable live working app

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   dependencies: {
+//     // '<dependency>': {
+//     // 'some-unsupported-package': {
+//     '': {
+//     // 'react-native.config': {
+//       platforms: {
+//         android: null, // disable Android platform, other platforms will still autolink
+//       },
+//     },
+//   },
+// };
